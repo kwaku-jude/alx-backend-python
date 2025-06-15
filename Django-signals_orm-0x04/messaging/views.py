d2b4ad7ab8f555ed32c734a67629a3884664bc94
Tanson Jude Antwi <tanson.aj@gmail.com>
2025-06-15 21:59:19 +0000

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -68,3 +68,49 @@ def unread_messages(request):
         'unread_messages': unread_messages
     }
     return render(request, 'messaging/unread_messages.html', context)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=['delete'], url_path='custom-delete')
+    def delete_user(self, request, pk=None):
+        user = self.get_object()
+        user.delete()
+        return Response({"message": "User deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        return Message.objects.filter(sender=self.request.user)
+
+    @method_decorator(cache_page(60))
+    def get_conversation(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+    @action(detail=False, methods=['get'])
+    def unread(self, request):
+        unread_messages = Message.unread.unread_for_user(request.user).only('sender_id', 'content', 'timestamp').select_related('sender')
+        serializer = self.get_serializer(unread_messages, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def inbox(request):
+            messages = Message.objects.filter(sender=request.user).select_related('sender', 'receiver').prefetch_related('replies')
+            serializer = MessageSerializer(messages, many=True)
+            return JsonResponse(serializer.data, safe=False)
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+    return Notification.objects.filter(recipient=self.request.user)
+
+
+class MessageHistoryViewSet(viewsets.ModelViewSet):
+    queryset = MessageHistory.objects.all()
+    serializer_class = MessageHistorySerializer
